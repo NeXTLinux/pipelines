4660d988633ac47617d8bfb5fdefbcaae5bc7b49
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-04-21 10:02:33 -0700

    Create command_linux.go

---
+++ golang/pipe/command_linux.go
@@ -0,0 +1,25 @@
+//go:build linux
+
+package pipe
+
+import (
+	"context"
+	"errors"
+
+	"github.com/nextlinux/pipeline/golang/internal/ptree"
+)
+
+// On linux, we can limit or observe memory usage in command stages.
+var _ LimitableStage = (*commandStage)(nil)
+
+var (
+	errProcessInfoMissing = errors.New("cmd.Process is nil")
+)
+
+func (s *commandStage) GetRSSAnon(ctx context.Context) (uint64, error) {
+	if s.cmd.Process == nil {
+		return 0, errProcessInfoMissing
+	}
+
+	return ptree.GetProcessTreeRSSAnon(s.cmd.Process.Pid)
+}
